4b301358eb2634ede9977931aa129af314022529
Tumaini Maganiko <tumainimaganiko@gmail.com>
2024-01-23 10:15:12 +0200

    Use Navigation

--- movie-app/components/trendingMovies.tsx
+++ movie-app/components/trendingMovies.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Dimensions, StyleSheet, Text, View} from "react-native";
 import Carousel from 'react-native-snap-carousel';
 import MovieCard from "./movieCard";
+import {useNavigation} from "@react-navigation/native";
 
 type TrendingMoviesProps = {
     trendingMovies: number[];
@@ -10,12 +11,18 @@ type TrendingMoviesProps = {
 const { width, height } = Dimensions.get('window');
 
 const  TrendingMovies: React.FC<TrendingMoviesProps> = ({trendingMovies}) => {
+
+    const navigation = useNavigation<any>();
+
+    const handleClick = () => {
+        navigation.navigate('Movies');
+    }
     return (
         <View style={styles.container}>
             <Text style={styles.trendingHeader}>Trending</Text>
             <Carousel
                 data={trendingMovies}
-                renderItem={({item}) => <MovieCard movieItem={item} />}
+                renderItem={({item}) => <MovieCard movieItem={item} handleClick={handleClick}/>}
                 firstItem={1}
                 inactiveSlideOpacity={.60}
                 sliderWidth={width}
