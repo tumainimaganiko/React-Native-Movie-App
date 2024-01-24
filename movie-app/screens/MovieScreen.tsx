9eae6d98dc508fda3fb8764b717f715a5d9e1930
Tumaini Maganiko <tumainimaganiko@gmail.com>
2024-01-24 08:49:31 +0200

    Create MovieScreen Component

---
+++ movie-app/screens/MovieScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text} from "react-native";
+
+const MovieScreen = () => {
+    return (
+        <Text>Movies Screen</Text>
+    )
+}
+
+export default MovieScreen;
