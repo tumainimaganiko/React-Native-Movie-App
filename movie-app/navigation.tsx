e5bf2b708d11264afd5546972eddbf58338c5fd5
Tumaini Maganiko <tumainimaganiko@gmail.com>
2024-01-24 08:49:50 +0200

    Use MovieScreen Component

--- movie-app/navigation.tsx
+++ movie-app/navigation.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeSreen from "./screens/HomeSreen";
+import MovieScreen from "./screens/MovieScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -12,6 +13,7 @@ export default function Navigation (){
                 headerShown: false
             }}>
                 <Stack.Screen name="Home" component={HomeSreen} />
+                <Stack.Screen name="Movies" component={MovieScreen} />
     </Stack.Navigator>
     </NavigationContainer>
 );
